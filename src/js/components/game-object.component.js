e199608391f11d65f456eae652eb5e3ed31a2005
Timmy Kokke <mental_c@hotmail.com>
2019-05-02 22:58:52 +0200

    added basic objects

---
+++ src/js/components/game-object.component.js
@@ -0,0 +1,33 @@
+export default AFRAME.registerComponent('s-game-object', {
+    schema: {
+        type: {
+            oneOf: ['start-laser', 'end-laser', 'mirror', 'wall'],
+            schemaChange: true,
+            default: 'wall'
+        },
+        position: {
+            type: 'vec3'
+        },
+        rotation: {
+            default: 'N',
+            oneOf: ['N', 'E', 'S', 'W'],
+            schemaChange: true
+        }
+    },
+
+    init: function () {
+        this.el.setAttribute("position", this.data.position);
+        this.el.setAttribute("mixin",`type-${this.data.type}`);
+        // switch (this.data.type.toLowerCase()) {
+        //     case "start-laser":
+                
+        //         break;
+        //     case "end-laser":
+        //         break;
+        //     case "mirror":
+        //         break;
+        //     case "wall":
+        //         break;
+        // };
+    },
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 import pixelMaterial from './components/pixel-material.component';
+import gameObject from './components/game-object.component';
 
 console.log('OfflineVR is online'); 
